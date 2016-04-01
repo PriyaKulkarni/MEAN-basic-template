e9a88483a2d81f5d83646aa625cbb9a7cc9de635
Priya Kulkarni <priya.bk18@gmail.com>
2016-04-01 17:27:16 +0530

    Angular setup file

---
+++ public/app.js
@@ -0,0 +1,7 @@
+//Here goes your angular setup code
+
+//create a module
+var app = angular.module('app',[]);
+
+//create a controller and define different functions of  the application
+//======================================================================
