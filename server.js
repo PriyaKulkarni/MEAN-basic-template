9627e4ceb5e8df5b3ec6c52f3ab36ecca79fe360
Priya Kulkarni <priya.bk18@gmail.com>
2016-04-05 00:09:14 +0530

    Comment added

--- server.js
+++ server.js
@@ -23,9 +23,11 @@ var express = require('express');
 
 app.use(express.static(__dirname + '/public'));			// set static files location /public
 
+// load HTML view file
 app.get('/', function(req, res) {
 	res.redirect('/index.html');
 });
+
 // listen (start app with node server.js) 
 app.listen(8080);
 console.log("App listening on port 8080");
